e02b9119c31ce0d83e6ea617fb2ae0d4238bfbc9
arturjkeee <artur.stepanyan.03@mail.ru>
2022-10-25 16:00:29 +0300

    Fix AI

--- The escape/Assets/_Scripts/Mechanics/AIMoving/AIMoving.cs
+++ The escape/Assets/_Scripts/Mechanics/AIMoving/AIMoving.cs
@@ -9,23 +9,20 @@ public class AIMoving : MonoBehaviour
 
     [SerializeField] private Transform StartPoint;
     [SerializeField] private Transform EndPoint;
+    [SerializeField] private float speed = 10;
+
     private bool moveDirecton = true;
-    void Start()
-    {
-        StartPoint.position = transform.position;
-        EndPoint.position = transform.position + new Vector3(5.0f,0,0);
-    }
 
     void FixedUpdate()
     {
         if (moveDirecton)
         {
-            transform.position = Vector3.MoveTowards(transform.position, EndPoint.position, 0.13f);
+            transform.position = Vector3.MoveTowards(transform.position, EndPoint.position, speed*Time.fixedDeltaTime);
             if (transform.position == EndPoint.position) { moveDirecton = false; }
         }
         else
         {
-            transform.position = Vector3.MoveTowards(transform.position, StartPoint.position, 0.13f);
+            transform.position = Vector3.MoveTowards(transform.position, StartPoint.position, speed * Time.fixedDeltaTime);
             if (transform.position == StartPoint.position) { moveDirecton = true; }
         }
 
